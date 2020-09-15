9be35ef8e0af82751f30277f2fbfbc1d88130269
Abdul Khan <akhan17@outlook.com>
2020-09-15 17:24:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ function moveDodgerLeft() {
 
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
-    let left = parseInt(leftNumbers, 10);
   }
 }
 
 function moveDodgerRight(){
   let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
 }
 
 document.addEventListener("keydown", function(e) {
