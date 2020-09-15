245f3de2db34cc3cf01fb7730947b8fb1960fea1
Abdul Khan <akhan17@outlook.com>
2020-09-15 17:24:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight(){
   let leftNumbers = dodger.style.right.replace("px", "");
   let left = parseInt(leftNumbers, 10);
   if (left > 0) {
-    dodger.style.left = `${left + 1}px`;
+    dodger.style.right = `${left + 1}px`;
   }
 }
 
