4dbdc60a92b1d043b40666a6705849abcadba1a0
Abdul Khan <akhan17@outlook.com>
2020-09-15 17:19:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 console.log(dodger.style.left); // "180px"
 console.log(dodger.style.bottom); // "0px"
-dodger.style.bottom = "0px";
-document.addEventListener("keydown", function(e) {
-  console.log(e.key);
-});
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px", "");
